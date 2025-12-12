7505572c049f64c0009058c2816c11f964c1313f
koszyk99 <krzysztof_koszyk@interia.eu>
2025-12-12 11:34:11 +0100

    confirm password

--- create_user.sh
+++ create_user.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 
-# create user login and checks whether a login was has been provided
+# create user login
 read -p "Add login for new user: " newuser
-    if [ -n "$newuser" ]; then
+    if [ -n "$newuser" ]; then # checks whether a login was has been provided
         if id "$newuser" &>/dev/null; then # check if the user already exist
             echo "User exist!"
         exit 1
-    else
-         echo "User does not exist."
     fi
         echo "Your login is: ${newuser}."
     else
@@ -18,11 +16,31 @@ read -p "Add login for new user: " newuser
 
 # create user password
 read -s -p "Add password for ${newuser}: " password
+    if [ -n "$password" ]; then  # check whether variable isn't empty
+        echo "Your password is: ${password}"
+    else
+        echo "You didn't provide password."
+        exit 1
+    fi
 
-# check whether variable isn't empty
-if [ -n "$password" ]; then
-    echo "Your password is: ${password}"
-else
-    echo "You didn't provide password."
-    exit 1
-fi
+# check password for validity
+max_attempt=2
+attempt=1
+
+while [ $attempt -le $max_attempt ]; do
+
+    read -s -p "Confirm your password: " password2
+    echo
+
+    if [ "$password" = "$password2" ]; then
+        echo "Password confirmed."
+        break
+    else
+        echo "Invalid password. Attempt $attempt of $max_attempt."
+    fi
+
+    attempt=$(( attempt + 1 ))
+
+done
+
+
