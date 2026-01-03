2afca2367aa809c31236845136ad633e36d05324
koszyk99 <krzysztof_koszyk@interia.eu>
2026-01-03 21:52:44 +0100

    create rest of the functions and create while loop

--- system_info.sh
+++ system_info.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # colors
-RED="\[\e[31m\]"
-GREEN="\[\e[32m\]"
-YELLOW="\[\e[33m\]"
-BLUE="\[\e[34m\]"
-RESET_COLOR="\[\e[0m\]"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+RESET_COLOR="\e[0m"
 
 # pause function
 pause(){
@@ -38,7 +38,45 @@ host_name(){
 
 disk_usage(){
     echo -e "${YELLOW}Disk usage:${RESET_COLOR}"
+    df -h /
 }
 
+ram(){
+    echo -e "${YELLOW}RAM information:${RESET_COLOR}"
+    free -m
+}
+
+network(){
+    echo -e "${YELLOW}Network:${RESET_COLOR}"
+    ip addr show
+}
+
+system_operation_time(){
+    echo -e "${YELLOW}System operation time:${RESET_COLOR}"
+    uptime
+}
+
+exit_program(){
+    echo -e "${YELLOW}Exit${RESET_COLOR}"
+    exit
+}
+
+while true; do
+    show_menu
+    read -p "Select your choice [ 1 - 7 ]: " choice
+
+    case $choice in
+        1) system_info ;;
+        2) host_name ;;
+        3) disk_usage ;;
+        4) ram ;;
+        5) network ;;
+        6) system_operation_time ;;
+        7) exit_program ;;
+        *) echo -e "${RED}Wrong choice! Select numbers from 1 to 7${RESET_COLOR}" ;;
+    esac
+
+    pause
+done
 
 
