e4b14bec450f946a273b3145579358ffb8eddbf4
koszyk99 <krzysztof_koszyk@interia.eu>
2026-04-07 18:17:37 +0200

    create resource_monitoring_system.sh

---
+++ resource_monitoring_system.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get RAM usage
+ram_usage() {
+    free -m | grep Mem | awk '{print $3/$2 * 100}' | cut -d. -f1
+}
+
+while true; do
+
+    usage=$(ram_usage)
+
+    if [ "$usage" -gt 80 ]; then
+        echo "Low RAM! Current usage: $usage%"
+    fi
+
+    sleep 5
+done
