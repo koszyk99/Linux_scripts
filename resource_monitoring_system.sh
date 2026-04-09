e261a6ac9a74f3976d97ca5d73027c5f4cbc8d2b
koszyk99 <krzysztof_koszyk@interia.eu>
2026-04-09 15:11:41 +0200

    feat: update resource script and add monitoring.log

--- resource_monitoring_system.sh
+++ resource_monitoring_system.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+# Create file to save the result
+LOG_FILE="./monitoring.log"
+
 # Get RAM usage
 ram_usage() {
     free -m | grep Mem | awk '{print $3/$2 * 100}' | cut -d. -f1
 }
 
-echo "Current RAM usage $(ram_usage)%"
+monitoring_system() {
+    local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+    local val=$1
+
+    echo "$timestamp - Memory: $val%" >> "$LOG_FILE"
+}
+
+echo "Monitoring started... (Press Ctrl+C to stop)"
 
 while true; do
 
     usage=$(ram_usage)
+
+    monitoring_system "$usage"
 
     if [ "$usage" -gt 80 ]; then
         echo "Low RAM! Current usage: $usage%"
